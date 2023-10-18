3132c773a2acbce2ec198f35f2eff1104837502f
Noah Schwenold <schwenoldnoah@gmail.com>
2023-10-18 14:36:29 +0200

    first commit

--- src/main/kotlin/de.nogaemer.minecraft/Main.kt
+++ src/main/kotlin/de.nogaemer.minecraft/Main.kt
@@ -1,10 +1,12 @@
 package de.nogaemer.minecraft
 
 import de.miraculixx.kpaper.main.KPaper
+import org.bukkit.Bukkit
 
 class Main: KPaper() {
 
     override fun startup() {
         Updater().update()
+        println("test")
     }
 }
